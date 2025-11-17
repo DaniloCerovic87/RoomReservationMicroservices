6bf93162691c0c88d26fd755bdad2997ce4a1fb9
Danilo <chessteacher87@gmail.com>
2025-11-17 21:06:48 +0100

    Authentication Service - added constraint for password to have between 8 and 64 characters

--- AuthenticationService/src/main/java/com/roomreservation/authenticationservice/dto/LoginRequest.java
+++ AuthenticationService/src/main/java/com/roomreservation/authenticationservice/dto/LoginRequest.java
@@ -1,6 +1,7 @@
 package com.roomreservation.authenticationservice.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
 
@@ -8,6 +9,7 @@ public record LoginRequest(
         String username,
 
         @NotBlank
+        @Size(min = 8, max = 64)
         String password
 
 ) {}
