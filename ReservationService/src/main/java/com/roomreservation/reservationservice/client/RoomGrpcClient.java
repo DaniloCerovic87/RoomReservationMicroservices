f8e55a53812fe4059a1d3eed0e88d9b0588f0350
Danilo <chessteacher87@gmail.com>
2026-01-02 08:00:35 +0100

    ReservationService -> RoomService implementing JWT in gRPC to improve security

--- ReservationService/src/main/java/com/roomreservation/reservationservice/client/RoomGrpcClient.java
+++ ReservationService/src/main/java/com/roomreservation/reservationservice/client/RoomGrpcClient.java
@@ -3,22 +3,31 @@ package com.roomreservation.reservationservice.client;
 import com.roomreservation.contracts.room.grpc.GetRoomSummariesRequest;
 import com.roomreservation.contracts.room.grpc.RoomGrpcServiceGrpc;
 import com.roomreservation.contracts.room.grpc.RoomSummary;
+import com.roomreservation.reservationservice.interceptor.JwtGrpcClientInterceptor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class RoomGrpcClient {
 
     @GrpcClient("roomService")
     private RoomGrpcServiceGrpc.RoomGrpcServiceBlockingStub stub;
+
+    private final JwtGrpcClientInterceptor jwtGrpcClientInterceptor;
 
     public List<RoomSummary> getRoomSummaries(List<Long> ids) {
         var req = GetRoomSummariesRequest.newBuilder()
                 .addAllRoomIds(ids)
                 .build();
 
-        return stub.getRoomSummaries(req).getRoomsList();
+        var securedStub = stub.withInterceptors(jwtGrpcClientInterceptor);
+        return securedStub.getRoomSummaries(req).getRoomsList();
     }
+
 }
